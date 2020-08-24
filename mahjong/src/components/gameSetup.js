b36d9a6b0d06740b4f94e92151aa948ce191aef9
Kevin <kevinchikitcheung@gmail.com>
2020-08-23 22:42:49 -0500

    added 144 tile wall array

---
+++ mahjong/src/components/gameSetup.js
@@ -0,0 +1,14 @@
+const tile = require('./tiles')
+
+makeWall = function(obj){
+    for(const tile in obj){
+        if(obj[tile] >= 34){
+                tileWall.push(tile)	
+        } else {
+            for(let i = 0; i < 4; i++){
+                tileWall.push(tile)
+        }
+    }
+}
+console.log(tileWall.flat())
+};
