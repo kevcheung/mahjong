bbfa2fa53cf2d79035c930146d4a81a6996d7d67
kevinchikitcheung@gmail.com <kevinchikitcheung@gmail.com>
2020-11-24 17:22:27 -0600

    added makeWall and drawFromWall

--- mahjong/src/components/gameSetup.js
+++ mahjong/src/components/gameSetup.js
@@ -1,16 +1,23 @@
 const tile = require('./tiles')
 
+let tileWall = [];
+
 makeWall = function(obj){
-    for(const tile in obj){
-        if(obj[tile] >= 34){
-                tileWall.push(tile)	
-        } else {
-            for(let i = 0; i < 4; i++){
-                tileWall.push(tile)
+        for(const tile in obj){
+            if(obj[tile] >= 34){
+                    tileWall.push(tile)
+            //if the value of tite is greater than or equal to 34, push tile to tilewall	
+            } else {
+                for(let i = 0; i < 4; i++){
+                    tileWall.push(tile)
+            }
+            //else push tile in 4 times
         }
     }
-}
-console.log(tileWall.flat())
+    console.log(tileWall.flat())
 };
 
-test
+drawFromWall = function(arr){
+	const randomInt = arr.length * Math.random() << 0;
+	console.log(randomInt)
+}
